d551354db0f9ccc30a693a0cb2442d8285661e10
unknown <xoliqberdiyevbehruz12@gmail.com>
2024-07-09 10:08:31 +0500

    feat(config) change swagger url

--- config/urls.py
+++ config/urls.py
@@ -34,7 +34,7 @@ urlpatterns = [
 
     # packages
     path('ckeditor/', include('ckeditor_uploader.urls')),
-    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
 
